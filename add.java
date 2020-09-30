dc39cb9c4fa303f3e93a03988f495eeec7b91cf0
Mohit <mohitsingh11082002@gmail.com>
2020-09-30 16:54:05 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class add {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        System.out.println(x-y);
+
+    }
+
+}
